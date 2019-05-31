9b691dab49e78844002092283600c71bd3d50767
Giaco <giacocipo@gmail.com>
2019-05-31 02:33:25 -0300

    Prueva 02

---
+++ test/fiuba/algo3/holaMundo/unoMasUnoIgualDosTest.java
@@ -0,0 +1,16 @@
+package fiuba.algo3.holaMundo;
+
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class unoMasUnoIgualDosTest {
+
+    @Test
+    public void test01esIgualAdos() {
+
+        assertEquals(1+1, 2);
+    }
+}
