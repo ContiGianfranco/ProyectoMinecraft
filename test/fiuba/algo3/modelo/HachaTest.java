449a95794f9462c5573ca63906bc4b56fc735755
kinirochan <kinirochan909@gmail.com>
2019-06-01 22:04:13 -0300

    primera parte logica vacia

---
+++ test/fiuba/algo3/modelo/HachaTest.java
@@ -0,0 +1,20 @@
+package fiuba.algo3.modelo;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class HachaTest {
+
+    @Test
+    public void test01SeCreaUnHachaDeMaderaConDurabilidad100YConFuerza2() {
+
+    }
+
+    @Test
+    public void test02SeCreaUnHachaDeMetalConDurabilidad400YConFuerza10() {
+
+    }
+
+
+}
---
+++ test/fiuba/algo3/modelo/PicoTest.java
@@ -0,0 +1,23 @@
+package fiuba.algo3.modelo;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class PicoTest {
+
+    @Test
+    public void test01SeCreaUnPicoDeMaderaConDurabilidad100YFuerza2() {
+
+    }
+
+    @Test
+    public void test02SeCreaUnPicoDeMetalConDurabilidad400YFuerza12() {
+
+    }
+
+    @Test
+    public void test03SeCreaUnPicoFinoConDurabilidad1000YFuerza20() {
+
+    }
+}
